e5cd4a88935d450b02ad8a018652f58841ee36b2
Luca Passone <luca.passone@gmail.com>
2011-06-20 13:46:24 +0300

    removed old code and reformatted some parts

--- SeisSol_setup/pickpoint_distribution.c
+++ SeisSol_setup/pickpoint_distribution.c
@@ -64,49 +64,34 @@ int main(int argc, char *argv[]){
 	
 	double x, y, z;
 	int nx, ny, nz;
-	/*
-	int x, y, z;
-	double current_x = min_x, current_y = min_y, current_z = min_z;
-	for (x = 0; x < n_points_x; x++){
-		current_y = min_y;
-		for (y = 0; y < n_points_y; y++){
-			current_z = min_z;
-			for (z = 0; z < n_points_z; z++){
-				printf("%f %f %f\n", current_x, current_y, current_z);
-				current_z += distance_z;
-			}
-			current_y += distance_y;
-		}
-		current_x += distance_x;
-	}*/
-		int i = 1;
-		nz = 0;
-		for(z = min_z; z <= max_z && nz<n_points_z ; z += distance_z){
-			ny = 0;
-			for (y = min_y; y <= max_y; y+= distance_y){
-				nx = 0;
-				for (x = min_x; x <= max_x; x+= distance_x){
-					if (z!=0.0) {
-						printf("%f %f %f\n", x, y, z);
-					} else {
-						printf("%f %f -0.1\n", x, y);
-					}
-					i++;
-					nx++;
+	int i = 1;
+	nz = 0;
+	for(z = min_z; z <= max_z && nz<n_points_z ; z += distance_z){
+		ny = 0;
+		for (y = min_y; y <= max_y; y+= distance_y){
+			nx = 0;
+			for (x = min_x; x <= max_x; x+= distance_x){
+				if (z!=0.0) {
+					printf("%f %f %f\n", x, y, z);
+				} else {
+					printf("%f %f -0.1\n", x, y);
 				}
-				ny++;
-				if (nx != n_points_x) {
-					printf("NX = %i", nx);
-				}
+				i++;
+				nx++;
 			}
-			nz++;
-			if (ny != n_points_y) {
-				printf("NY = %i", ny);
+			ny++;
+			if (nx != n_points_x) {
+				printf("NX = %i", nx);
 			}
 		}
-		if (nz != n_points_z){
-			printf("NZ = %i", nz);
+		nz++;
+		if (ny != n_points_y) {
+			printf("NY = %i", ny);
 		}
+	}
+	if (nz != n_points_z){
+		printf("NZ = %i", nz);
+	}
 	
 	return 0;
 }
